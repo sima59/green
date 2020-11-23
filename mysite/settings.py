23f3b590340d8d7e85a9e72ce5c4ad33b75a68ec
sima59 <simabageri@gmail.com>
2020-11-22 18:18:32 -0600

    changed root back

--- mysite/settings.py
+++ mysite/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'posts/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # MEDIA/UPLOADS
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
